b80c5eecc490c3007248615207929ec1adb89bd9
PaolaP <paolaperez8@gmail.com>
2021-11-15 23:04:51 -0500

    input full string from ngrok from the 4 to the end wthout :

--- net_example/client.js
+++ net_example/client.js
@@ -1,17 +1,18 @@
 /** @format */
 
+const net = require("net");
 const conn = net.createConnection({
 	host: "localhost", // change to IP address of computer or ngrok host if tunneling
-	port: 18930, // or change to the ngrok port if tunneling
+	port: 17441, // or change to the ngrok port if tunneling
 });
-
+//when running ngrok this will give you the port and the host
 conn.setEncoding("utf8"); // interpret data as text
 // client.js
-conn.on("data", (data) => {
-	console.log("Server says: ", data);
-});
-// client.js
+// conn.on("data", (data) => {
+// 	console.log("Server says: ", data);
+// });
+// // client.js
 
-conn.on("connect", () => {
-	conn.write("Hello from client!");
-});
+// conn.on("connect", () => {
+// 	conn.write("Hello from client!");
+// });
--- net_example/server.js
+++ net_example/server.js
@@ -9,12 +9,12 @@ server.listen(3000, () => {
 });
 
 // server.js
-// add this line after server is created, before listen is called
+// // add this line after server is created, before listen is called
 server.on("connection", (client) => {
 	console.log("New client connected!");
 	client.write("Hello there!");
 });
-client.setEncoding("utf8"); // interpret data as text
-client.on("data", (data) => {
-	console.log("Message from client: ", data);
-});
+// client.setEncoding("utf8"); // interpret data as text
+// client.on("data", (data) => {
+// 	console.log("Message from client: ", data);
+// });
